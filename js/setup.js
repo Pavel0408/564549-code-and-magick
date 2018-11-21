b8fc71edc7b887f38bc00f5e078e017cd52951e4
Pavel <pavel0408@gmail.com>
2018-11-21 20:33:45 +0300

    показал попап, добавил массивы свойств волшебников

---
+++ js/setup.js
@@ -0,0 +1,18 @@
+'use strict';
+
+//  функция для показа попапа
+var setup = document.querySelector('.setup');
+var setupOpen = function () {
+  setup.classList.remove('hidden');
+};
+
+var wizardsNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+
+var wizardsSurnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+
+var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+
+var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+
+//  показываем попап
+setupOpen();
