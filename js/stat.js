22c7b7a1b7138a1618120d12c5530a23e68a214e
Pavel <pavel0408@gmail.com>
2018-12-14 14:39:18 +0300

    перенёс конастанты из модуля статистики в модуль констант

--- js/stat.js
+++ js/stat.js
@@ -1,22 +1,10 @@
 'use strict';
 (function () {
-  var CLOUD_WIDTH = 420;
-  var CLOUD_HEIGHT = 270;
-  var CLOUD_X = 100;
-  var CLOUD_Y = 10;
-  var SHADOW_GAP = 10;
-  var TITLE_GAP_X = 60;
-  var HISTOGRAM_Y = 265;
-  var HISTOGRAM_HEIGHT = 150;
-  var YOUR_NAME = 'Вы';
-  var HISTROGRAM_GAP = 10;
-  var RECT_WIDTH = 40;
-  var HISTOGRAM_COLUMN_WDTH = 90;
 
   //  функция отрисовки облака
   var renderCloud = function (ctx, x, y, color) {
     ctx.fillStyle = color;
-    ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+    ctx.fillRect(x, y, window.constants.CLOUD_WIDTH, window.constants.CLOUD_HEIGHT);
   };
 
   //  ищем максимальный элемент в массиве
@@ -41,20 +29,20 @@
 
   window.renderStatistics = function (ctx, players, times) {
     var playersLength;
-    var histogramX = CLOUD_X + 50;
+    var histogramX = window.constants.CLOUD_X + 50;
     var titleGapY = 20;
     var maxTime;
     var rectHeigth;
 
     //  рисуем облако
-    renderCloud(ctx, CLOUD_X + SHADOW_GAP, CLOUD_Y + SHADOW_GAP, 'rgba(0, 0, 0, 0.7)');
-    renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+    renderCloud(ctx, window.constants.CLOUD_X + window.constants.SHADOW_GAP, window.constants.CLOUD_Y + window.constants.SHADOW_GAP, 'rgba(0, 0, 0, 0.7)');
+    renderCloud(ctx, window.constants.CLOUD_X, window.constants.CLOUD_Y, '#fff');
 
     //  функция для вывода заголовка
     var makeTitle = function (text) {
       ctx.font = '16px PT Mono';
       ctx.fillStyle = '#000';
-      ctx.fillText(text, CLOUD_X + TITLE_GAP_X, CLOUD_Y + titleGapY);
+      ctx.fillText(text, window.constants.CLOUD_X + window.constants.TITLE_GAP_X, window.constants.CLOUD_Y + titleGapY);
       titleGapY += titleGapY;
     };
 
@@ -67,15 +55,15 @@
 
     //  рисуем гистограму
     for (var i = 0; i < playersLength; i++) {
-      rectHeigth = HISTOGRAM_HEIGHT * times[i] / maxTime;
+      rectHeigth = window.constants. HISTOGRAM_HEIGHT * times[i] / maxTime;
       ctx.fillStyle = '#000';
-      ctx.fillText(players[i], histogramX, HISTOGRAM_Y);
-      ctx.fillText(Math.round(times[i]), histogramX, HISTOGRAM_Y - rectHeigth - HISTROGRAM_GAP * 3);
+      ctx.fillText(players[i], histogramX, window.constants.HISTOGRAM_Y);
+      ctx.fillText(Math.round(times[i]), histogramX, window.constants.HISTOGRAM_Y - rectHeigth - window.constants.HISTROGRAM_GAP * 3);
 
-      ctx.fillStyle = players[i] === YOUR_NAME ? 'rgba(255, 0, 0, 1)' : getRandomBlue();
+      ctx.fillStyle = players[i] === window.constants.YOUR_NAME ? 'rgba(255, 0, 0, 1)' : getRandomBlue();
 
-      ctx.fillRect(histogramX, HISTOGRAM_Y - HISTROGRAM_GAP * 2 - rectHeigth, RECT_WIDTH, rectHeigth);
-      histogramX += HISTOGRAM_COLUMN_WDTH;
+      ctx.fillRect(histogramX, window.constants.HISTOGRAM_Y - window.constants.HISTROGRAM_GAP * 2 - rectHeigth, window.constants.RECT_WIDTH, rectHeigth);
+      histogramX += window.constants.HISTOGRAM_COLUMN_WDTH;
     }
   };
 })();
